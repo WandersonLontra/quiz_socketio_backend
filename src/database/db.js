b6754755a928a915c5825f871905dcb3af2536c3
WandersonLontra <wandersonlontra@yahoo.com.br>
2021-12-15 02:39:45 -0300

    Criação do server e conexão com websocket

---
+++ src/database/db.js
@@ -0,0 +1,24 @@
+
+export const questions = [
+    {
+        id: "5410d36b-02d8-489b-906d-50794e2f5f54",
+        question_about: "Descobrimento do Brasil",
+        question:"Quem descobriu o Brasil?",
+        options: ["Os indíos", "Cristovão Colombo", "Pero Vaz de Caminha", "Pedro Alvares Cabral", "Dom Pedro"],
+        answer: ["Pedro Alvares Cabral"]
+    },
+    {
+        id: "46316dda-faeb-4478-8a82-26eee9b186df",
+        question_about: "Números Pares",
+        question: "Quais desses números são pares?",
+        options: ["10", "55", "382", "101", "9"],
+        answer: ["10","382"]
+    },
+    {
+        id: "4c912c5a-14e9-43c3-8321-5999728bea57",
+        question_about: "Números Ímpares",
+        question: "Quais desses números são ímpares?",
+        options: ["646", "20", "421", "101", "9"],
+        answer: ["421","101","9"]
+    }
+]
---
+++ src/server.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import http from 'http';
+import { Server as ioServer } from 'socket.io';
+import { v4 as uuidv4 } from 'uuid';
+
+import { questions } from './database/db.js';
+
+
+const app = express();
+const server = http.createServer(app);
+
+
+export const io = new ioServer(server, {
+    cors: {
+        origin: process.env.ORIGIN_CORS_URL,
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on('connection', socket => {
+    console.log(`Socket connected with ${socket.id}`);
+
+    socket.emit('storageQuestions', questions);
+
+    socket.on('createQuestion', question => {
+        if(question !== null){
+            const keys = Object.keys(question);
+
+            for (let key of keys) {
+                if (question[key] == "") {
+                    return
+                }
+            }
+
+            const newQuestion = {
+                id: uuidv4(),
+                ...question
+            }
+
+            questions.push(newQuestion);
+
+            // console.log('DEPOIS:',questions);
+
+            socket.broadcast.emit('receivedQuestions',newQuestion);
+        }
+    });
+
+    socket.on('sendMessage',username => console.log(username));
+});
+
+const port = process.env.PORT || 3333;
+
+server.listen(port, () => console.log('Node Server is connected on port:',port))
